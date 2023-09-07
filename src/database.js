3796537cdfa71a6fb999c2da28f06d5681af515a
Bruno Freijomil <bruno_freijomil@hotmail.com>
2023-09-07 17:51:49 -0300

    FIX: Conexion a la base de datos de MongoDB

--- src/database.js
+++ src/database.js
@@ -1,8 +1,8 @@
 // Este archivo tiene la conexión a la base de datos.
 const mongoose = require("mongoose")
-const { NOTES_APP_MONGODB_HOS, NOTES_APP_MONGODB_DATABASE } = process.env
+const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE } = process.env
 
-const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOS}/${NOTES_APP_MONGODB_DATABASE}`
+const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`
 
 mongoose.connect(MONGODB_URI, {
     useUnifiedTopology: true,
