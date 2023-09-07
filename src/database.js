0ca9b59f43a42f1840660af4c64547f0158175d2
Bruno Freijomil <bruno_freijomil@hotmail.com>
2023-09-07 17:49:50 -0300

    Conexion a la base de datos de MongoDB

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose")
 const { NOTES_APP_MONGODB_HOS, NOTES_APP_MONGODB_DATABASE } = process.env
 
-const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOS}//${NOTES_APP_MONGODB_DATABASE}`
+const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOS}/${NOTES_APP_MONGODB_DATABASE}`
 
 mongoose.connect(MONGODB_URI, {
     useUnifiedTopology: true,
