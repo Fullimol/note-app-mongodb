f8434af7fd251819bb35b30a52b500a4c925c368
Bruno Freijomil <bruno_freijomil@hotmail.com>
2023-09-07 18:45:00 -0300

    Se crea motor de Handlebars.

--- src/server.js
+++ src/server.js
@@ -1,13 +1,23 @@
 // este archivo solo tiene el código del servidor.
-const express = require('express')
+const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require("path")
 
 // Inicializaciones
 const app = express()
 
 // Settings
-app.set('port', process.env.PORT || 8080);
-app.set('views', path.join(__dirname, 'views')) // para que busque la carpeta "views"
+app.set('port', process.env.PORT || 8080); //Si en ".env" no se encuentra la url del server, usar el localhost.
+app.set('views', path.join(__dirname, 'views')) //Para que busque la carpeta "views".
+
+app.engine('.hbs', exphbs.engine({//Crea motor de handlebars.
+    defaultLayout: "main",
+    //"path.join" se utiliza para unir rutas de directorio, se está uniendo la ruta al directorio de vistas con la cadena.
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}))
+app.set("view engine", ".hbs")
 
 // Middlewares
 app.use(express.urlencoded({ extended: false })) //convierte los datos que se reciben en un objeto .JSON
@@ -16,7 +26,7 @@ app.use(express.urlencoded({ extended: false })) //convierte los datos que se re
 
 // Routes
 app.get("/", (req, res) => {
-    res.send("Hola mundo!")
+    res.render("index")
 })
 
 // Statics files (archivos que cualquier cliente puede acceder desde el servidor)
